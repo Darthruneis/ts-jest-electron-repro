a12ff7dcdad5f78597f1b987f2c76f48e69f982f
Chris Thompson <Darthruneis@hotmail.com>
2021-11-24 13:49:29 -0500

    Set up typescript support.
    - followed [this github repo](https://github.com/electron/electron-quick-start-typescript)

---
+++ dist/main.js
@@ -0,0 +1,29 @@
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+var path = require("path");
+var createWindow = function () {
+    var win = new electron_1.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        }
+    });
+    win.loadFile(path.join(__dirname, "../index.html"));
+    win.webContents.openDevTools();
+};
+electron_1.app.whenReady().then(function () {
+    createWindow();
+    electron_1.app.on("window-all-closed", function () {
+        if (process.platform !== "darwin") {
+            electron_1.app.quit();
+        }
+    });
+    electron_1.app.on("activate", function () {
+        if (electron_1.BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+//# sourceMappingURL=main.js.map
